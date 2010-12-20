60b76da5ee913f92559bd23069a9c0448d475cf3
mpr.gap@gmail.com <mpr.gap@gmail.com@a2eec048-235d-f308-7ccb-d22129e654cd>
2010-12-20 20:09:33 +0000

    first working version of reading gpx files from ZIPs

--- src/org/opengpx/lib/xml/ZipFileReader.java
+++ src/org/opengpx/lib/xml/ZipFileReader.java
@@ -2,10 +2,12 @@ package org.opengpx.lib.xml;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 
@@ -14,7 +16,9 @@ import java.util.zip.ZipFile;
  */
 public class ZipFileReader 
 {
-	
+
+	private static final Logger mLogger = LoggerFactory.getLogger(ZipFileReader.class);
+
 	/**
 	 * 
 	 */
@@ -36,30 +40,25 @@ public class ZipFileReader
 			for (Enumeration e = zipFile.entries(); e.hasMoreElements();) 
 			{
 				final ZipEntry zipEntry = (ZipEntry) e.nextElement();
-				System.out.println("File name: " + zipEntry.getName() + "; size: " + zipEntry.getSize() + "; compressed size: " + zipEntry.getCompressedSize());
+				mLogger.debug("File name: " + zipEntry.getName() + "; size: " + zipEntry.getSize() + "; compressed size: " + zipEntry.getCompressedSize());
 				final InputStream inputStream = zipFile.getInputStream(zipEntry);
-				InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 
-				// char[] buffer = new char[(int) zipEntry.getSize()];
-				// final String bufferAsString = new String(buffer);
-				// bufferAsString.getBytes();
-				
-				char[] buffer = new char[1024];
-				String s = new String();
-				while (inputStreamReader.read(buffer, 0, buffer.length) != -1) 
+				final String zipEntryName = zipEntry.getName().toLowerCase();
+				if (zipEntryName.endsWith("gpx") || zipEntryName.endsWith("loc"))
 				{
-					s = s.concat(new String(buffer));
-				}
-				
-				if (zipEntry.getName().toLowerCase().endsWith("gpx"))
-				{
-					final GPXFileReader gpxFileReader = new GPXFileReader();
-					gpxFileReader.readByteArray(s.trim().getBytes());
-				} 
-				else if (zipEntry.getName().toLowerCase().endsWith("loc"))
-				{
-					final LOCFileReader locFileReader = new LOCFileReader();
-					locFileReader.readByteArray(s.trim().getBytes());
+					final byte[] buffer = new byte[(int) zipEntry.getSize()];
+					inputStream.read(buffer, 0, buffer.length);
+	
+					if (zipEntry.getName().toLowerCase().endsWith("gpx"))
+					{
+						final GPXFileReader gpxFileReader = new GPXFileReader();
+						gpxFileReader.readByteArray(buffer);
+					} 
+					else if (zipEntry.getName().toLowerCase().endsWith("loc"))
+					{
+						final LOCFileReader locFileReader = new LOCFileReader();
+						locFileReader.readByteArray(buffer);
+					}
 				}
 			}
 			return true;
